aba912334d5d85584715a692049512bbeac5e504
jmeredith8 <jmeredith059@gmail.com>
2026-06-17 09:16:14 -0400

    Update app.R
    Updated dashboard version with more interactive features and fixes to map display.

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 ############## NAIAN Biomass Data Map Dashboard
 
 ### Setting working directory to folder in R drive
-setwd("R:/Ryan_Lab/Jakob_M/Projects/NAIAN_Dash")
+# setwd("R:/Ryan_Lab/Jakob_M/Projects/NAIAN_Dash")
 
 ### Loading in libraries
 library(shiny)
@@ -90,21 +90,39 @@ plot_data <- plot_data %>%
 plot_data <- plot_data %>%
   mutate(plot_data, tot_biomass = (((10^(plot_data$Log10_Tot_Biomass_Day)) - 0.001) * (plot_data$sample_days)))
 
-plot_data_agg <- plot_data %>% 
-  group_by(year, location, state) %>% 
-  summarise(
-    tot_biomass = round(mean(tot_biomass, na.rm = TRUE), 3),
-    temp = round(mean(temp, na.rm = TRUE), 2),
-    wind = round(mean(wind, na.rm = TRUE), 2),
-    precip = round(mean(precip, na.rm = TRUE), 2),
-    alt = mean(alt, na.rm = TRUE),
-    month = first(month),
-    date = first(date),
-    unique_id = first(unique_id),
-    long_j = mean(long_j),
-    lat_j = mean(lat_j),
-    .groups = "drop"
-  )
+plot_data <- plot_data %>% 
+  mutate(tot_biomass = round(tot_biomass, 3),
+         temp = round(temp, 2),
+         wind = round(wind, 2),
+         precip = round(precip, 2))
+
+plot_data <- plot_data %>% 
+  group_by(location, state, year) %>% 
+  arrange(Date_1May, .by_group = TRUE) %>% 
+  mutate(collection_num = row_number()) %>% 
+  ungroup()
+
+plot_data <- plot_data %>% 
+  mutate(map_label = paste0(location, ", ", state))
+
+plot_data <- plot_data %>% 
+  mutate(region = gsub("^[0-9]+_", "", region))
+
+# plot_data_agg <- plot_data %>% 
+#   group_by(year, location, state) %>% 
+#   summarise(
+#     tot_biomass = round(mean(tot_biomass, na.rm = TRUE), 3),
+#     temp = round(mean(temp, na.rm = TRUE), 2),
+#     wind = round(mean(wind, na.rm = TRUE), 2),
+#     precip = round(mean(precip, na.rm = TRUE), 2),
+#     alt = mean(alt, na.rm = TRUE),
+#     month = first(month),
+#     date = first(date),
+#     unique_id = first(unique_id),
+#     long_j = mean(long_j),
+#     lat_j = mean(lat_j),
+#     .groups = "drop"
+#   )
 
 ### Establishing UI
 ui <- navbarPage("NAIAN Dashboard",
@@ -156,17 +174,33 @@ ui <- navbarPage("NAIAN Dashboard",
                    sidebarLayout(
                      sidebarPanel(
                        
+                       p("Select a year of collections to view"),
+                       
+                       selectInput(
+                         inputId = "year_select",
+                         label = "Select a Year",
+                         choices = c(
+                           "Select a time..." = "",
+                           "2019" = "2019",
+                           "2020" = "2020",
+                           "2021" = "2021"
+                         ),
+                         selected = ""
+                      ),
+                       
                        p("Select a variable to view a color-coded version of the map"),
                        
                        radioButtons(
                          inputId = "color_by",
                          label = "Color Markers By:",
                          choices = c(
-                           "Year" = "year",
+                           "Month" = "month",
                            "Temperature" = "temp",
                            "Wind Speed" = "wind",
                            "Precipitation" = "precip",
-                           "Altitude" = "alt"
+                           "Altitude" = "alt",
+                           "Biomass" = "tot_biomass", 
+                           "Collection Number" = "collection_num"
                          ),
                          selected = "none"
                        ),
@@ -185,6 +219,93 @@ ui <- navbarPage("NAIAN Dashboard",
                        uiOutput("hover_biomass")
                      )
                    )
+                 ),
+                 
+                 tabPanel(
+                   "Trends Graphs",
+                   titlePanel("Trends Graphs"),
+                   sidebarLayout(
+                     sidebarPanel(
+
+                       p("Select a year of biomass trends to view for a specific site"),
+
+                       selectInput(
+                         inputId = "year_select",
+                         label = "Select a Year",
+                         choices = c(
+                           "Select a time..." = "",
+                           "2019" = "2019",
+                           "2020" = "2020",
+                           "2021" = "2021"
+                         ),
+                         selected = ""
+                       ),
+                       
+                       conditionalPanel(
+                         condition = "input.year_select != ''",
+                         selectInput(
+                           inputId = "site_select",
+                           label = "Select a Site",
+                           choices = NULL,
+                           selected = NULL
+                         )
+                       ),
+
+                     ),
+                     
+                    mainPanel(
+                      fluidRow(
+                        style = "margin-top: 50px;",
+                        column(
+                          width = 10, 
+                          offset = 1, 
+                          plotOutput("trend_chart", height = 600))
+                      )
+                    )
+                   ),
+                    
+                    sidebarLayout(
+                      sidebarPanel(
+                        
+                        p("Select a year of biomass trends to view for a specific site"),
+                        
+                        selectInput(
+                          inputId = "year_select_quad",
+                          label = "Select a Year",
+                          choices = c(
+                            "Select a time..." = "",
+                            "2019" = "2019",
+                            "2020" = "2020",
+                            "2021" = "2021"
+                          ),
+                          selected = ""
+                        ),
+                        
+                        radioButtons(
+                          inputId = "charts_display",
+                          label = "Compare Biomass with:",
+                          choices = c(
+                            "Temperature" = "temp",
+                            "Wind Speed" = "wind",
+                            "Precipitation" = "precip",
+                            "Altitude" = "alt"
+                          ),
+                          selected = "none"
+                        ),
+                        
+                      ),
+                      
+                      mainPanel(
+                        fluidRow(
+                          style = "margin-top: 50px;",
+                          column(
+                            width = 10, 
+                            offset = 1, 
+                            plotOutput("quad_chart", height = 600))
+                        )
+                      )
+                     
+                   )
                  )
 )
 
@@ -193,7 +314,14 @@ server <- function(input, output, session) {
   
   output$map <- renderLeaflet({
     color_by <- input$color_by
-    n <- nrow(plot_data_agg)
+    
+    filtered_data <- if(input$year_select == "") {
+      plot_data
+    } else {
+      plot_data %>% filter(year == as.numeric(input$year_select))
+    }
+    
+    n <- nrow(filtered_data)
     
     map <- leaflet(options = leafletOptions(preferCanvas = FALSE)) %>%
       addTiles() %>%
@@ -202,7 +330,7 @@ server <- function(input, output, session) {
     if (is.null(color_by) || color_by == "none" || n == 0) {
       return(
         map %>% addCircleMarkers(
-          data = plot_data_agg,
+          data = filtered_data,
           lng = ~long_j,
           lat = ~lat_j,
           radius = 4,
@@ -215,60 +343,53 @@ server <- function(input, output, session) {
           layerId = ~as.character(unique_id),
           # layerId = ~paste0(lat_j, "_", long_j),
           label = lapply(paste0(
-            "<b>Location:</b> ", plot_data_agg$location, ", ", plot_data_agg$state, "<br>",
-            "<b>Year:</b> ", plot_data_agg$year),
+            "<b>Location:</b> ", filtered_data$map_label, "<br>",
+            "<b>Year:</b> ", filtered_data$year),
             HTML)
         )
       )
     }
     
-    values <- plot_data_agg[[color_by]]
+    values <- filtered_data[[color_by]]
     values[is.na(values) | trimws(values) == ""] <- "Unknown"
     
     unique_vals <- unique(values)
     
-    numeric_vals <- as.numeric(values[values != "Unknown"])
-    
-    breaks <- classIntervals(numeric_vals, n = 5, style = "jenks")$brks
-    
-    pal <- colorBin(
-      palette = plasma(5),
-      domain = as.numeric(values),
-      bins = breaks,
-      na.color = "grey"
-    )
-    
-    if (color_by == "year") {
+    if (color_by == "month") {
       pal <- colorFactor(palette = plasma(length(unique_vals)), domain = unique_vals)
     } else {
-      breaks <- classIntervals(as.numeric(values[values != "Unknown"]), n = 5, style = "jenks")$brks
+      numeric_vals <- as.numeric(values[values != "Unknown"])
+      breaks <- classIntervals(numeric_vals, n = 5, style = "jenks")$brks
       pal <- colorBin(palette = plasma(5), domain = as.numeric(values), bins = breaks, na.color = "grey")
     }
     
     legend_title <- switch(color_by,
                            "year" = "Year",
-                           "temp" = "Temperature",
-                           "wind" = "Wind Speed",
-                           "precip" = "Precipitation",
-                           "alt" = "Altitude")
+                           "temp" = "Temperature (C)",
+                           "wind" = "Wind Speed (km/hr)",
+                           "precip" = "Precipitation (mm)",
+                           "alt" = "Altitude (m)",
+                           "tot_biomass" = "Biomass (g)",
+                           "collection_num" = "Collection Number")
 
   map %>%
     addCircleMarkers(
-      data = plot_data_agg,
+      data = filtered_data,
       lng = ~long_j,
       lat = ~lat_j,
       radius = ~pmax(rescale(tot_biomass, to = c(2, 20)), 2),
       color = "black",
-      weight = 0.5,
-      fillColor = ~pal(as.numeric(values)), 
+      weight = 2,
+      fillColor = ~pal(filtered_data[[color_by]]), 
       fillOpacity = 0.8,
       stroke = TRUE,
       options = pathOptions(interactive = TRUE),
       layerId = ~as.character(unique_id),
       # layerId = ~paste0(lat_j, "_", long_j),
       label = lapply(paste0(
-        "<b>Location:</b> ", plot_data_agg$location, "<br>",
-        "<b>Year:</b> ", plot_data_agg$year),
+        "<b>Location:</b> ", filtered_data$map_label, "<br>",
+        "<b>Date:</b> ", filtered_data$month, " ", filtered_data$date, ", ", filtered_data$year, "<br>",
+        "<b>Collection:</b> ", filtered_data$collection_num),
         HTML)
     ) %>%
     addLegend(
@@ -288,7 +409,7 @@ observeEvent(input$map_marker_mouseover, {
   print(paste("SHAPE mouseover fired, id:", event$id))
   # Establishing source of hover text data
   if(!is.null(event$id)) {
-    check_data <- plot_data_agg[plot_data_agg$unique_id == as.integer(event$id), ]
+    check_data <- plot_data[plot_data$unique_id == as.integer(event$id), ]
     print(paste("rows matched:", nrow(check_data)))
     if(nrow(check_data) > 0) {
       
@@ -302,13 +423,18 @@ observeEvent(input$map_marker_click, {
   click <- input$map_marker_click
   
   
+  # # Identify the row by matching the lat/long of the clicked marker
+  # row <- plot_data %>%
+  #   filter(abs(lat_j - click$lat) < 0.01, abs(long_j - click$lng) < 0.01) %>%
+  #   slice(1)
+  # print(paste("rows found:", nrow(row)))
+  # print(row)
+  
   # Identify the row by matching the lat/long of the clicked marker
-  row <- plot_data_agg %>%
-    filter(abs(lat_j - click$lat) < 0.01, abs(long_j - click$lng) < 0.01) %>%
-    slice(1)
+  row <- plot_data %>%
+    filter(unique_id == as.integer(click$id))
   print(paste("rows found:", nrow(row)))
   print(row)
-  
   
   output$click_info <- renderUI({
     print("inside renderUI")
@@ -317,7 +443,7 @@ observeEvent(input$map_marker_click, {
       
       div(
         style = "padding: 15px; background-color: #f9f9f9; border-radius: 8px; border: 1px solid #ddd;",
-        h3(paste0(row$location, ", ", row$state)),
+        h3(paste0(row$map_label)),
         tags$table(
           style = "width: 100%; font-size: 16px;",
           tags$tr(
@@ -343,7 +469,11 @@ observeEvent(input$map_marker_click, {
           tags$tr(
             tags$td(style = "font-weight: bold; padding: 5px;", "Total Biomass:"),
             tags$td(style = "padding: 5px;", row$tot_biomass, " g")
-          )
+          ),
+          tags$tr(
+            tags$td(style = "font-weight: bold; padding: 5px;", "Collection Number:"),
+            tags$td(style = "padding: 5px;", row$collection_num)
+          ),
         )
       )
       
@@ -356,30 +486,110 @@ observeEvent(input$map_marker_click, {
     })
   })
 })
+
+observeEvent(input$year_select, {
+  req(input$year_select != "")
   
+  sites <- plot_data %>% 
+    filter(year == as.numeric(input$year_select)) %>% 
+    pull(map_label) %>% 
+    unique() %>% 
+    sort()
+  
+  updateSelectInput(session, "site_select", choices = sites, selected = sites[1])
+})
+
+output$trend_chart <- renderPlot({
+  
+  if (is.null(input$site_select) || input$site_select == "" ||
+      is.null(input$year_select) || input$year_select == "") {
+    return(
+      ggplot() +
+        labs(title = "Select a year and variable for comparing with biomass trends by region",
+             x = "Day of Year", y = "Total Biomass") +
+        theme_minimal() +
+        theme(plot.title = element_text(hjust = 0.5, color = "grey60", size = 16))
+    )
+  }
+  
+  chart_data <- plot_data %>% 
+    filter(map_label == input$site_select,
+           year == as.numeric(input$year_select)) %>% 
+    arrange(Date_1May)
+  
+  if (nrow(chart_data) == 0) {
+    return(ggplot() + labs(title = "No data") + theme_void())
+  }
+  
+  ggplot(chart_data, aes(x = factor(date), y = tot_biomass)) +
+    geom_col(fill = "steelblue", color = "white") +
+    geom_smooth(aes(x = as.numeric(factor(date))),
+                method = "loess", se = FALSE, color = "orange", linewidth = 3) +
+    labs(
+      title = paste("Biomass Over Time - ", input$site_select, input$year_select),
+      x = "Date (1 = May 1st)", 
+      y = "Total Biomass (mg)") +
+    theme_minimal() +
+    theme(
+      axis.text.x = element_text(angle = 45, hjust = 1, size = 12),
+      axis.title.x = element_text(size = 15),
+      axis.title.y = element_text(size = 15),
+      axis.text.y = element_text(size = 15),
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 20)
+    )
+})
+
+output$quad_chart <- renderPlot({
+  x_var <- input$charts_display
+  
+  if (is.null(x_var) || x_var == "none") {
+    return(
+      ggplot() +
+        labs(title = "Select a variable to compare with biomass") +
+        theme_void() +
+        theme(plot.title = element_text(hjust = 0.5, color = "grey60", size = 16))
+    )
+  }
+  
+  chart_data2 <- if (is.null(input$year_select_quad) || input$year_select_quad == "") {
+    plot_data
+  } else {
+    plot_data %>% filter(year == as.numeric(input$year_select_quad))
+  }
+  
+  if (nrow(chart_data2) == 0) {
+    return(ggplot() + labs(title = "No data for this selection") + theme_void())
+  }
+  
+  x_label <- switch(x_var,
+                    "temp" = "Temperature (C)",
+                    "wind" = "Wind Speed (km/hr)",
+                    "precip" = "Precipitation (mm)",
+                    "alt" = "Altitude (m)")
+  
+  ggplot(chart_data2, aes(x = .data[[x_var]], y = tot_biomass)) +
+    geom_point(aes(color = region), alpha = 0.6, size = 2) +
+    geom_smooth(method = "loess", se = TRUE, color = "black", linewidth = 0.8) +
+    facet_wrap(~ region, scales = "free") +
+    scale_color_manual(values = rainbow(length(unique(chart_data2$region)))) +
+    labs(
+      title = paste("Biomass vs ", x_label),
+      x = x_label,
+      y = "Total Biomass"
+    ) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 18),
+      axis.title = element_text(size = 13),
+      axis.text = element_text(size = 11),
+      strip.text = element_text(face = "bold", size = 12),
+      legend.position = "none"
+    )
+})
+
 }
 
 shinyApp(ui, server)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
